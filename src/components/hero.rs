e94d9ed193de960696d1a2115a6868dbb02466c7
Spheniscine <spheniscine@gmail.com>
2026-06-17 20:32:53 +0800

    web test

--- src/components/hero.rs
+++ src/components/hero.rs
@@ -6,65 +6,69 @@ use crate::{components::CardComponent, game::{Card, HonorSkin, Skin, Suit, SuitS
 #[component]
 pub fn Hero() -> Element {
     let width = 11f32;
-    let skin = Skin { honors: HonorSkin::Eastern, suits: SuitSkin::Eastern };
     rsx! {
         div {
             id: "hero",
+            for (y, skin) in [(0., Skin { honors: HonorSkin::Eastern, suits: SuitSkin::Eastern }), 
+                (70., Skin { honors: HonorSkin::Mythical, suits: SuitSkin::Animals })] {
             
-            for i in 1..=9 {
+                
+                for i in 1..=9 {
+                    CardComponent { 
+                        position: Vec2::new(3., y + 6. * i as f32),
+                        width,
+                        card: Card::Number { rank: i, suit: Suit::Red },
+                        skin,
+                    }
+                }
+
+                for i in 1..=9 {
+                    CardComponent { 
+                        position: Vec2::new(15., y + 6. * i as f32),
+                        width,
+                        card: Card::Number { rank: i, suit: Suit::Green },
+                        skin,
+                    }
+                }
+
+                for i in 1..=9 {
+                    CardComponent { 
+                        position: Vec2::new(27., y + 6. * i as f32),
+                        width,
+                        card: Card::Number { rank: i, suit: Suit::Blue },
+                        skin,
+                    }
+                }
+
                 CardComponent { 
-                    position: Vec2::new(3., 6. * i as f32),
+                    position: Vec2::new(39., y + 6.),
                     width,
-                    card: Card::Number { rank: i, suit: Suit::Red },
+                    card: Card::Honor { suit: Suit::Red },
+                    skin,
+                }
+
+                CardComponent { 
+                    position: Vec2::new(51., y + 6.),
+                    width,
+                    card: Card::Honor { suit: Suit::Green },
+                    skin,
+                }
+
+                CardComponent { 
+                    position: Vec2::new(63., y + 6.),
+                    width,
+                    card: Card::Honor { suit: Suit::Blue },
+                    skin,
+                }
+
+                CardComponent { 
+                    position: Vec2::new(39., y + 20.),
+                    width,
+                    card: Card::Flower,
                     skin,
                 }
             }
-
-            for i in 1..=9 {
-                CardComponent { 
-                    position: Vec2::new(15., 6. * i as f32),
-                    width,
-                    card: Card::Number { rank: i, suit: Suit::Green },
-                    skin,
-                }
-            }
-
-            for i in 1..=9 {
-                CardComponent { 
-                    position: Vec2::new(27., 6. * i as f32),
-                    width,
-                    card: Card::Number { rank: i, suit: Suit::Blue },
-                    skin,
-                }
-            }
-
-            CardComponent { 
-                position: Vec2::new(39., 6.),
-                width,
-                card: Card::Honor { suit: Suit::Red },
-                skin,
-            }
-
-            CardComponent { 
-                position: Vec2::new(51., 6.),
-                width,
-                card: Card::Honor { suit: Suit::Green },
-                skin,
-            }
-
-            CardComponent { 
-                position: Vec2::new(63., 6.),
-                width,
-                card: Card::Honor { suit: Suit::Blue },
-                skin,
-            }
-
-            CardComponent { 
-                position: Vec2::new(39., 20.),
-                width,
-                card: Card::Flower,
-                skin,
-            }
+            
         }
     }
 }
